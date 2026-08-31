14c5bde5321f31778f33ec84852077ca851ad241
Anthony GARGOUA <gargouaanthony@hotmail.fr>
2026-08-31 14:11:24 +0200

    tp 02 : Injection de dependances

---
+++ src/main/java/fr/diginamic/hello/controleurs/HelloService.java
@@ -0,0 +1,13 @@
+package fr.diginamic.hello.controleurs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    @Bean
+    public String salutations(){
+        return "Je suis la classe de service et je vous dis Bonjour ";
+    }
+}
